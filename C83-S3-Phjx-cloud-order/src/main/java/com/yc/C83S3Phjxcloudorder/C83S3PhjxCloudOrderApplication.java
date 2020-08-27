0d7ec252a73996eb1dad42f9a96429d43a5012ac
风吹来的小仙女 <@DESKTOP-S7B2RQ0>
2020-08-27 10:34:06 +0800

    实现远程服务互访

--- C83-S3-Phjx-cloud-order/src/main/java/com/yc/C83S3Phjxcloudorder/C83S3PhjxCloudOrderApplication.java
+++ C83-S3-Phjx-cloud-order/src/main/java/com/yc/C83S3Phjxcloudorder/C83S3PhjxCloudOrderApplication.java
@@ -2,12 +2,22 @@ package com.yc.C83S3Phjxcloudorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class C83S3PhjxCloudOrderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(C83S3PhjxCloudOrderApplication.class, args);
+	}
+	
+	/**
+	 * 定义RestTemplate Bean
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- C83-S3-Phjx-cloud-user/src/main/java/com/yc/C83S3Phjxclouduser/C83S3PhjxCloudUserApplication.java
+++ C83-S3-Phjx-cloud-user/src/main/java/com/yc/C83S3Phjxclouduser/C83S3PhjxCloudUserApplication.java
@@ -2,12 +2,22 @@ package com.yc.C83S3Phjxclouduser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class C83S3PhjxCloudUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(C83S3PhjxCloudUserApplication.class, args);
+	}
+	
+	/**
+	 * 定义RestTemplate Bean
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
