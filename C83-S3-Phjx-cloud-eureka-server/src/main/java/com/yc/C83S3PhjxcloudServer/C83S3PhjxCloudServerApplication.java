a898df66001f3d3ee7b2215af341541beb96d946
风吹来的小仙女 <@DESKTOP-S7B2RQ0>
2020-08-27 11:13:48 +0800

    配置Eureka服务器

---
+++ C83-S3-Phjx-cloud-eureka-server/src/main/java/com/yc/C83S3PhjxcloudServer/C83S3PhjxCloudServerApplication.java
@@ -0,0 +1,19 @@
+package com.yc.C83S3PhjxcloudServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class C83S3PhjxCloudServerApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(C83S3PhjxCloudServerApplication.class, args);
+	}
+	
+	
+}
